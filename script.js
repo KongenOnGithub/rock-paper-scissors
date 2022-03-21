1041a932f6a8c7f22bb020d2b2fa1361a697d48b
KongenOnGithub <michaeldj100@gmail.com>
2022-03-21 17:22:35 +0100

    Add the game function

--- script.js
+++ script.js
@@ -7,11 +7,8 @@ function userSelect() {
     let looping = true;
     let userSelection = prompt("Which one do you choose? rock paper or scissors");
     userSelection = userSelection.toLowerCase();
-    console.log(userSelection);
-    console.log(looping);
     if(userSelection==="rock" || userSelection==="paper" || userSelection==="scissors") {
         looping = false;
-        console.log(looping);
     }
 
     while(looping===true){
@@ -76,4 +73,34 @@ function playRound(userSelection, computerSelection) {
     }
 
 
+}
+
+function game() {
+    let userScore = 0;
+    let computerScore=0;
+
+    for (let i=0; i<5; i++){
+        let userChoice = userSelect();
+        let computerChoice = computerPlay();
+        let result = playRound(userChoice, computerChoice)
+        console.log(result);
+        if(result.substring(0, 8)==="You win!") {
+            userScore += 1;
+        }
+        else if(result.substring(0,9)==="You lose!") {
+            computerScore += 1;
+        }
+        
+    }
+
+    if(userScore>computerScore) {
+        console.log(`You won the best of five with a score of ${userScore} to ${computerScore}`);
+    }
+    else if(computerScore>userScore) {
+        console.log(`You lost the best of five with a score of ${userScore} to ${computerScore}`);
+    }
+
+    else {
+        console.log(`The best of five ended in a tie with a score of ${userScore} to ${computerScore}`);
+    }
 }
