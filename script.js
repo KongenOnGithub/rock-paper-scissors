4b9bafa77dd38ebb5b24e70f547c35430e56cc5b
KongenOnGithub <michaeldj100@gmail.com>
2022-03-21 16:13:18 +0100

    Add playRound and userSelect functions

--- script.js
+++ script.js
@@ -2,3 +2,69 @@ function computerPlay() {
     let options = ["rock", "paper", "scissors"]
     return options[Math.floor(Math.random()*options.length)];
 }
+
+function userSelect() {
+    let userSelection = prompt("Which one do you choose? rock paper or scissors");
+    userSelection = userSelection.toLowerCase;
+
+
+    while(userSelection!=="rock" && userSelection!=="paper" && userSelection!=="scissors"){
+        userSelection = prompt("Try again, remember you can only choose between rock paper or scissors");
+        userSelection = userSelection.toLowerCase;
+    }
+
+    return userSelection;
+
+}
+
+     
+   
+
+
+function playRound(userSelection, computerSelection) {
+    if (userSelection==="rock") {
+        if(computerSelection==="rock"){
+        return "It's a tie! you both chose rock";
+        }
+        else if(computerSelection==="paper"){
+        return "You lose! paper beats rock";
+        }
+        else if(computerSelection==="scissors"){
+        return "You win! rock beats scissors";
+        }
+        else {
+        return "i think something went wrong"
+        }
+    }
+    else if (userSelection==="paper") {
+        if(computerSelection==="rock"){
+        return "You win! paper beats rock";
+        }
+        else if(computerSelection==="paper"){
+        return "It's a tie! you both chose paper";
+        }
+        else if(computerSelection==="scissors"){
+        return "You lose! scissors beat paper";
+        }
+        else {
+        return "i think something went wrong"
+        }
+    }
+
+    else if (userSelection==="scissors"){
+        if(computerSelection==="rock"){
+        return "You lose! rock beats scissors";
+        }
+        else if(computerSelection==="paper"){
+        return "You win! scissors beats paper";
+        }
+        else if(computerSelection==="scissors"){
+        return "It's a tie! you both chose scissors";
+        }
+        else {
+        return "i think something went wrong"
+        }
+    }
+
+
+}
